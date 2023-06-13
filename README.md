6d76bd45df4db5e0f851e98a232e1ac857f4435e
majharul224 <majharulislsm2225@gmail.com>
2023-06-13 21:48:38 +0600

    readme file add

---
+++ README.md
@@ -0,0 +1,8 @@
+### websiteName: fake gun 
+
+## live link : https://soft-bavarois-962db8.netlify.app/
+
+### I used react and vit
+### I used tailwind
+### I used daisyui
+### I used daisyui modal
--- src/Component/Modal/Modal.jsx
+++ src/Component/Modal/Modal.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
 const Modal = ({gunDetails}) => {
-    const {name}=gunDetails
+    const {name,price}=gunDetails
     return (
         <>
             <dialog id="my_modal_3" className="modal">
                 <form method="dialog" className="modal-box">
                     <button htmlFor="my-modal-3" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                     <h3 className="font-bold text-lg">{name}</h3>
-                    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                    <p className="py-4">Price- ${price}</p>
+                    <p className="py-4">Category- {category}</p>
+                    <p className="py-4">Bullet- {bullet}</p>
                 </form>
             </dialog>
         </>
